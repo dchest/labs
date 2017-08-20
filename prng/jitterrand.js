bb4b7e93e6bb64b517534cd301eaebb1472302ea
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-08-20 18:39:54 +0200

    jitterrand: collapse with addition mod 2^32; fix out of bounds read

--- prng/jitterrand.js
+++ prng/jitterrand.js
@@ -57,9 +57,9 @@ function jitterRand(out) {
 
         // Collapse and erase state,
         // leaving result in the first two indexes.
-        for (var i = 1; i < len; i += 2) {
-            s[0] ^= s[i + 0]; s[i + 0] = 0;
-            s[1] ^= s[i + 1]; s[i + 1] = 0;
+        for (var i = 1; i < len - 1; i += 2) {
+            s[0] = (s[0] + s[i + 0]) | 0; s[i + 0] = 0;
+            s[1] = (s[1] + s[i + 1]) | 0; s[i + 1] = 0;
         }
     }
 
