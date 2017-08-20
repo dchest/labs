ac4b8edbe834d861d886ab728479206cfd1d6b6d
Dmitry Chestnykh <dmitry@codingrobots.com>
2017-08-20 18:45:40 +0200

    jitterrand: fix state erasure
    Was erasing s[1].

--- prng/jitterrand.js
+++ prng/jitterrand.js
@@ -58,8 +58,10 @@ function jitterRand(out) {
         // Collapse and erase state,
         // leaving result in the first two indexes.
         for (var i = 1; i < len - 1; i += 2) {
-            s[0] = (s[0] + s[i + 0]) | 0; s[i + 0] = 0;
-            s[1] = (s[1] + s[i + 1]) | 0; s[i + 1] = 0;
+            s[0] = (s[0] + s[i + 0]) | 0;
+            s[1] = (s[1] + s[i + 1]) | 0;
+            s[i + 0] = 0;
+            s[i + 1] = 0;
         }
     }
 
